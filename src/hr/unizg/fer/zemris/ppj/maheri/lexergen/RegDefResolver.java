bec2d411c283fe336f4bbc66ba407995fd2f6683
dosvald <denis.osvald+github@gmail.com>
2012-10-19 01:53:44 +0200

    Tu zalijepiti razrijesavanje regularnih definicija

---
+++ src/hr/unizg/fer/zemris/ppj/maheri/lexergen/RegDefResolver.java
@@ -0,0 +1,25 @@
+package hr.unizg.fer.zemris.ppj.maheri.lexergen;
+
+import java.util.Map;
+
+/**
+ * can parse the input and return a Key-Value map with the regexes resolved
+ * 
+ * @author Aleksandar "Nisam-jos-gotov-s-12-linija-u-4-dana" Gavrilovic
+ * 
+ */
+public class RegDefResolver {
+
+	/**
+	 * Transforms the input into a key-value map
+	 * 
+	 * @param input
+	 *           The input to parse and resolve regexes
+	 * @return A key-value map
+	 */
+	public Map<String, String> parseRegexes(String[] array) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
