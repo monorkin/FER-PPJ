61aa23ba93c7116991ecb7104b77350fc8501eba
CroCoder <andrej.mrkonjic@gmail.com>
2012-10-20 01:17:54 +0300

    za ujutro ostatak,odo spati

--- src/hr/unizg/fer/zemris/ppj/maheri/lexergen/InputProcessor.java
+++ src/hr/unizg/fer/zemris/ppj/maheri/lexergen/InputProcessor.java
@@ -1,7 +1,9 @@
-package hr.unizg.fer.zemris.ppj.maheri.lexergen;
+//package hr.unizg.fer.zemris.ppj.maheri.lexergen;
 
-import hr.unizg.fer.zemris.ppj.maheri.lexergen.structs.LexerRuleDescriptionText;
+//import hr.unizg.fer.zemris.ppj.maheri.lexergen.structs.LexerRuleDescriptionText;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -9,29 +11,68 @@ import java.util.Map;
  * This class lists what must be provided for the lexer generator to
  * successfully do shit
  * 
- * @author dosvald
+ * @author dosvald, crocoder
  */
 public class InputProcessor {
 	
+	List<String> RegularDefinitions = new ArrayList<String>();
+	List<String> LexerStates = new ArrayList<String>();
+	List<String> TokenNames = new ArrayList<String>();
+	
+	/*
+	 * za ujutro: popunit mapu, testirat, osposobit eclipse egit
+	 * napit se popodne
+	 */
+	
 	public InputProcessor(List<String> inputLines) {
-		// TODO IMPLEMENT
+		Integer mode = 1;
 		//throw new RuntimeException("unimplemented");
+
+		 Iterator<String> it= inputLines.iterator();
+
+	     while(it.hasNext())
+	        {
+	         String value=(String)it.next();   
+	         if(value.charAt(0) == '%'){
+	        	  if(value.charAt(1) == 'X')
+	        		  mode = 2;
+	        	  else if(value.charAt(1) == 'L')
+	        		  mode = 3;
+	        	  else if((mode == 3) && (value.charAt(0) == '<'))
+	        	  {
+	        		  mode = 4;
+	        	  }
+	          }        
+	          switch (mode) {
+	            case 1:  
+	            		 RegularDefinitions.add(value); break;
+	            case 2: 
+	            	    value = value.substring(3, value.length());
+	            		for(String a: value.split(" ")){
+	            				LexerStates.add(a); }break;
+	            case 3:  
+            	        value = value.substring(3, value.length());
+            		    for(String b: value.split(" ")){
+            					TokenNames.add(b); }break;
+	          	}
+	          
+	        }
 	}
 
+	
+	
 	/**
 	 * @return list of regular definitions
 	 */
 	public List<String> getRegularDefinitions() {
-		// TODO IMPLEMENT
-		return null;
+		return RegularDefinitions;
 	}
 
 	/**
 	 * @return list of lexer states that will be used
 	 */
 	public List<String> getLexerStates() {
-		// TODO IMPLEMENT
-		return null;
+		return LexerStates;
 	}
 
 	/**
@@ -39,8 +80,7 @@ public class InputProcessor {
 	 *         input program into
 	 */
 	public List<String> getTokenNames() {
-		// TODO IMPLEMENT
-		return null;
+		return TokenNames;
 	}
 
 	/**
@@ -53,9 +93,9 @@ public class InputProcessor {
 	 *            according to the definition map. Otherwise the rules are kept
 	 *            verbatim
 	 */
-	public List<LexerRuleDescriptionText> getLexerRules(Map<String, String> regDef) {
-		// TODO IMPLEMENT
-		return null;
-	}
+//	public List<LexerRuleDescriptionText> getLexerRules(Map<String, String> regDef) {
+//		// TODO IMPLEMENT
+//		return null;
+//	}
 
 }
