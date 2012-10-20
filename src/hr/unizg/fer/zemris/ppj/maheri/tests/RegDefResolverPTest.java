d491c352c486c393fe01d1bb01d3042f21aa5b33
dosvald <denis.osvald+github@gmail.com>
2012-10-20 17:22:29 +0200

    Popravljen ovaj test, sad radi, na razini fail/pass

--- src/hr/unizg/fer/zemris/ppj/maheri/tests/RegDefResolverPTest.java
+++ src/hr/unizg/fer/zemris/ppj/maheri/tests/RegDefResolverPTest.java
@@ -1,6 +1,8 @@
 package hr.unizg.fer.zemris.ppj.maheri.tests;
 
-import hr.unizg.fer.zemris.ppj.maheri.gen.interfaces.RegexParser;
+import hr.unizg.fer.zemris.ppj.maheri.lexergen.RegDefResolver;
+
+import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -17,23 +19,16 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
-public abstract class RegexParserTest {
+public class RegDefResolverPTest {
 
 	private String[] input;
 	private String[] output;
-	private RegexParser parser;
 
-	public RegexParserTest(String[] input, String[] output) {
+
+	public RegDefResolverPTest(String[] input, String[] output) {
 		this.input = input;
 		this.output = output;
-		this.parser = getParser();
 	}
-
-	/**
-	 * Redefine this method in order to return the RegexParser implementation you
-	 * would like to test
-	 */
-	public abstract RegexParser getParser();
 
 	@Parameters
 	public static Collection<Object[]> data() throws IOException {
@@ -48,17 +43,17 @@ public abstract class RegexParserTest {
 
 	@Test
 	public void testOutputKeys() {
-		Map<String, String> map = parser.parseRegexes(input);
+		Map<String, String> map = RegDefResolver.parseRegexes(input);
 		for (String s : input) {
-			assert (map.keySet().contains(s.split(" ")[0]));
+			assertTrue (map.keySet().contains(s.split(" ")[0]));
 		}
 	}
 
 	@Test
 	public void testOutputValues() {
-		Map<String, String> map = parser.parseRegexes(input);
+		Map<String, String> map = RegDefResolver.parseRegexes(input);
 		for (String s : output) {
-			assert (map.values().contains(s.split(" ")[1]));
+			assertTrue (map.values().contains(s.split(" ")[1]));
 		}
 	}
 
