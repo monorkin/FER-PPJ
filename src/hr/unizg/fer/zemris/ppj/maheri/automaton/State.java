07dcea10e57c9510adbf37cebfd3cedef5361a7c
Petar Šegina <psegina@ymail.com>
2012-10-20 19:39:29 +0200

    getByName - a utility method useful for avoiding unnecessary instantiations

--- src/hr/unizg/fer/zemris/ppj/maheri/automaton/State.java
+++ src/hr/unizg/fer/zemris/ppj/maheri/automaton/State.java
@@ -1,44 +1,54 @@
 package hr.unizg.fer.zemris.ppj.maheri.automaton;
 
+import java.util.List;
 import java.util.Vector;
 
 public class State implements Comparable<State> {
 
-		protected String name;
-		protected Vector<Transition> transitions = new Vector<Transition>();;
-		protected Transition eTransition = null;
+	protected String name;
+	protected Vector<Transition> transitions = new Vector<Transition>();;
+	protected Transition eTransition = null;
 
-		public State(String name) {
-			this.name = name;
-		}
-
-		public boolean hasEpsilonTransition() {
-			return eTransition != null;
-		}
-
-		public Transition getEpsilonTransition() {
-			return this.eTransition;
-		}
-
-		@Override
-		public int compareTo(State arg0) {
-			return this.name.compareTo(arg0.name);
-		}
-
-		@Override
-		public boolean equals(Object arg0) {
-			if (arg0 instanceof String) {
-				return this.name.equals(arg0);
-			}
-			if (arg0 instanceof State) {
-				return this.name.equals(((State) arg0).name);
-			}
-			return false;
-		}
-
-		@Override
-		public String toString() {
-			return this.name;
-		}
-
+	public State(String name) {
+		this.name = name;
 	}
+
+	public boolean hasEpsilonTransition() {
+		return eTransition != null;
+	}
+
+	public Transition getEpsilonTransition() {
+		return this.eTransition;
+	}
+
+	public static State getByName(String stateName, List<State> stateList) {
+		for (State s : stateList) {
+			if (s.equals(stateName)) {
+				return s;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public int compareTo(State arg0) {
+		return this.name.compareTo(arg0.name);
+	}
+
+	@Override
+	public boolean equals(Object arg0) {
+		if (arg0 instanceof String) {
+			return this.name.equals(arg0);
+		}
+		if (arg0 instanceof State) {
+			return this.name.equals(((State) arg0).name);
+		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return this.name;
+	}
+
+}
