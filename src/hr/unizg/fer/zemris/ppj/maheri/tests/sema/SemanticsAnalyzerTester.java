b9d4ebd4837c6d3237626db2d42996d1ee556ede
tljubej <sporbie@gmail.com>
2012-12-22 01:13:04 +0100

    Izglancao sam tester malo, međutim zbiva se jedan jako čudan bug, pogledajte FIXME u testeru koji sam stavio

--- src/hr/unizg/fer/zemris/ppj/maheri/tests/sema/SemanticsAnalyzerTester.java
+++ src/hr/unizg/fer/zemris/ppj/maheri/tests/sema/SemanticsAnalyzerTester.java
@@ -10,7 +10,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -24,40 +24,59 @@ import static org.junit.Assert.*;
 @RunWith(Parameterized.class)
 public class SemanticsAnalyzerTester {
 	private String fileName;
-	
+
 	public SemanticsAnalyzerTester(String fileName) {
-		this.fileName=fileName;
+		this.fileName = fileName;
 	}
-	
+
 	@Parameters
 	public static List<String[]> getFileNames() {
-		File files= new File("res/examples/seman-in");
-		File[] listFiles= files.listFiles();
+		File files = new File("res/examples/seman-in/");
+		File[] listFiles = files.listFiles();
+		// FIXME Ovisno o tome da li je ova linija zakomentirana ili ne se
+		// testovi drukčije izvrte, ako je zakomentirana bude 5 failura ako
+		// odkomentirana onda 6, nema nikakvog smisla
+		 Arrays.sort(listFiles);
 		List<String[]> fileNames = new ArrayList<String[]>();
-		for (File f: listFiles) {
-			String fName=f.getName();
+		for (File f : listFiles) {
+			String fName = f.getName();
 			String[] arej = new String[1];
-			arej[0]= fName.substring(0, fName.length()-3);
+			arej[0] = fName.substring(0, fName.length() - 3);
 			fileNames.add(arej);
 		}
 		return fileNames;
 	}
-	
-	
+
 	@Test
-	public void superDuperTest() throws IOException {
-		System.out.println("\nTESTISUJEMO TEST PRIMJER: "+fileName);
-		String output=getAnalyzerOutputFromFile("res/examples/seman-in/"+fileName+".in");
-		String correctOutput=new Scanner(new File("res/examples/seman-out/"+fileName+".out")).useDelimiter("\\Z").next();
+	public void superDuperTest() throws Exception {
+		System.out.println("\nTESTISUJEMO TEST PRIMJER: " + fileName);
+		String output = null;
+		try {
+			output = getAnalyzerOutputFromFile("res/examples/seman-in/" + fileName + ".in");
+		} catch (Exception e) {
+			System.out.println("------------");
+			System.out.println("NEŠTO OŠLO U KURAC");
+			e.printStackTrace();
+			System.out.println("------------");
+			throw e;
+
+		}
+		String correctOutput = null;
+		try {
+			correctOutput = new Scanner(new File("res/examples/seman-out/" + fileName + ".out")).useDelimiter("\\Z")
+					.next();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 		System.out.println("------------");
-		System.out.println(output);
-		System.out.println(correctOutput);
+		System.out.println("DOBIVENO: " + output);
+		System.out.println("OČEKIVANO: " + correctOutput);
 		System.out.println("------------");
 		assertEquals(correctOutput, output);
 	}
-	
+
 	public String getAnalyzerOutputFromFile(String filename) throws IOException {
-		BufferedReader reader=null;
+		BufferedReader reader = null;
 		try {
 			reader = new BufferedReader(new FileReader(filename));
 		} catch (FileNotFoundException e) {
@@ -72,23 +91,25 @@ public class SemanticsAnalyzerTester {
 		reader.close();
 		InputProcessor ip = new InputProcessor(inputLines);
 		Node tree = ip.getTree();
-		
+
 		SemanticsAnalyzer analyzer = new SemanticsAnalyzer(tree);
-		
+
 		analyzer.checkAttributes();
-		
+
 		analyzer.checkFunctions();
-		
+
 		return analyzer.getOutput();
 	}
-	
-//	@Test
-//	public void simpleTest() throws IOException {
-//		String output=getAnalyzerOutputFromFile("res/examples/seman-in/01_idn.in");
-//		String correctOutput=new Scanner(new File("res/examples/seman-out/01_idn.out")).useDelimiter("\\Z").next();
-//		
-//		assertEquals(correctOutput, output);
-//		
-//
-//	}
+
+	// @Test
+	// public void simpleTest() throws IOException {
+	// String
+	// output=getAnalyzerOutputFromFile("res/examples/seman-in/01_idn.in");
+	// String correctOutput=new Scanner(new
+	// File("res/examples/seman-out/01_idn.out")).useDelimiter("\\Z").next();
+	//
+	// assertEquals(correctOutput, output);
+	//
+	//
+	// }
 }
